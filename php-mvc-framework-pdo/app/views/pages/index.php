bf45974a6fd3c4a5dfa140ed324637b36c46ebda
devrap2497 <74716974+devrap2497@users.noreply.github.com>
2022-03-24 10:17:30 +0800

    Delete php-mvc-framework-pdo/app directory

--- php-mvc-framework-pdo/app/views/pages/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-	/*echo SITENAME;*/
-	/*echo $data['title'];*/
-	foreach ($data['users'] as $user) {
-		echo "Username: " . $user->user_name;
-		echo "</br>";
-		echo "Email: " . $user->user_email;
-		echo "</br>";
-	}
